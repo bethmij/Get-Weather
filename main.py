19f0d89b6a49719f9a68867370fb6f0a0aa1ec1c
Dinarbrooke <bethmij@gmail.com>
2023-12-17 13:01:11 +0530

    get http response

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_weather():
+    latitude = 6.053519
+    longitude = 80.220978
+    api_key = "f7ea23e4c43dab84aca723334945185d"
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        json_response = json.loads(response.content)
+        print(json_response["weather"][0])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    get_weather()
