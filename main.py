a897a919ee85542586c476b3a54f4e20c07eea39
Dinarbrooke <bethmij@gmail.com>
2023-12-17 13:24:56 +0530

    update weatherCheck with thread

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ longitude = 80.220978
 api_key = "f7ea23e4c43dab84aca723334945185d"
 
 if __name__ == "__main__":
-    weatherCheck = WeatherCheck()
-    weatherCheck.get_weather(latitude, longitude, api_key)
+    weatherCheck = WeatherCheck(latitude, longitude, api_key)
+    weatherCheck.start()
+
+
--- weatherCheck.py
+++ weatherCheck.py
@@ -1,8 +1,22 @@
 import requests
 import json
+from threading import Thread
+import time
 
 
-class WeatherCheck:
+class WeatherCheck(Thread):
+
+    def __init__(self, latitude, longitude, api_key):
+        super(WeatherCheck,self).__init__()
+        self.latitude = latitude
+        self.longitude = longitude
+        self.api_key = api_key
+
+    def run(self):
+        while True:
+            self.get_weather(self.latitude, self.longitude, self.api_key)
+            time.sleep(5)
+
     def get_weather(self, latitude, longitude, api_key):
         url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
         response = requests.get(url)
