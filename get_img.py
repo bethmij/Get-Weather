94734fce33a32c009bf2912309afd1c76c3aed2b
Dinarbrooke <bethmij@gmail.com>
2023-12-23 21:52:03 +0530

    modified get_img.py

--- get_img.py
+++ get_img.py
@@ -6,14 +6,15 @@ import requests
 
 
 class GetImg(Thread):
-    def __init__(self, queries):
+    def __init__(self, queries, image):
         super(GetImg, self).__init__()
         self.queries = queries
         self.previous_query = None
+        self.image = image
 
     def run(self):
         queries = self.queries.get()
-        self.previous_query = queries
+        # self.previous_query = queries
         print("Query :", queries)
         while queries is not None:
             if self.previous_query == queries:
@@ -21,7 +22,8 @@ class GetImg(Thread):
                 print("Same Query")
                 continue
 
-            url = f'https://unsplash.com/napi/search/photos?page=1&query={queries}'
+            dsp = queries['description']
+            url = f'https://unsplash.com/napi/search/photos?page=1&query={dsp}'
 
             response = requests.get(url)
 
@@ -40,3 +42,20 @@ class GetImg(Thread):
     def __download_img(self, results):
         row_img = results['urls']['raw']
         print(row_img)
+        response = requests.get(row_img, stream=True)
+
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            x = row_img.split('?')
+            image_name = x[0].split('/')[-1]
+
+            path = f"image/{image_name}.jpg"
+            if response.status_code == 200:
+                with open(path, "wb") as f:
+                    f.write(response.content)
+                print("Image downloaded successfully.")
+                self.image.put(path)
+                return True
+            else:
+                print(f"Failed to download image. Status code: {response.status_code}")
+                return False
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from multiprocessing import Queue
+from queue import Queue
 
 from get_img import GetImg
 from weather_check import WeatherCheck
@@ -7,12 +7,13 @@ latitude = 6.053519
 longitude = 80.220978
 api_key = "f7ea23e4c43dab84aca723334945185d"
 queries = Queue()
+image =Queue()
 
 if __name__ == "__main__":
     weatherCheck = WeatherCheck(latitude, longitude, api_key, queries)
     weatherCheck.start()
 
-    getImage = GetImg(queries)
+    getImage = GetImg(queries, image)
     getImage.start()
 
     # for i in range(0, 10):
